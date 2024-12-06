0dd96c874d0dbfd2771204bcf5c203b93ffc11f4
Vítor Souza <vitorlegalvh@gmail.com>
2024-12-06 17:01:29 -0300

    separação das responsabilidades

--- src/controller/TeamController.java
+++ src/controller/TeamController.java
@@ -3,37 +3,22 @@ package controller;
 import java.util.logging.Logger;
 
 import model.Team;
-import repository.TeamRepository;
+import service.TeamService;
 
 public class TeamController {
     private static final Logger logger = Logger.getLogger(TeamController.class.getName());
-    private TeamRepository teamRepository;
+    private TeamService teamService;
 
     public TeamController() {
-        this.teamRepository = new TeamRepository();
+        this.teamService = new TeamService();
     }
 
     public void addTeam(Team team) {
-        if (isTeamExists(team)) {
+        if (teamService.isTeamExists(team)) {
             logger.severe("Erro: o time \"" + team.getName() + "\" já foi cadastrado.");
         } else {
-            int newId = teamRepository.getNewId();
-            teamRepository.getTeams().put(newId, team);
-            saveTeams();
+            teamService.addTeam(team);
             logger.info("Time \"" + team.getName() + "\" adicionado ao repositório.");
         }
-    }
-
-    private boolean isTeamExists(Team team) {
-        for (Team existingTeam : teamRepository.getTeams().values()) {
-            if (existingTeam.getName().equals(team.getName())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void saveTeams() {
-        teamRepository.saveData();
     }
 }
