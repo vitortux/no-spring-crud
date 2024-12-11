a153c3a9b80509469848dcf2cefadc46784e4f43
Vítor Souza <vitorlegalvh@gmail.com>
2024-12-11 14:48:17 -0300

    refatoração do controller e implementação dos menus (interface)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,75 @@
+import javax.swing.JOptionPane;
+
+import controller.TeamController;
+import repository.TeamRepository;
+import service.TeamService;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        // TODO: interface.
+        menu();
+    }
+
+    private static void menu() {
+        int option;
+        do {
+            String[] options = { "Times", "Sair" };
+            option = JOptionPane.showOptionDialog(null,
+                    "Escolha uma opção de gerenciamento:",
+                    "Menu",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    options,
+                    null);
+
+            switch (option) {
+                case 0:
+                    teamController();
+                    break;
+                case 1:
+                    System.exit(0);
+                    break;
+                default:
+                    break;
+            }
+        } while (option != -1);
+    }
+
+    private static void teamController() {
+        TeamController controller = new TeamController(new TeamService(new TeamRepository()));
+
+        int option;
+        do {
+            String[] options = { "Adicionar time", "Listar times", "Buscar time", "Atualizar time", "Deletar time",
+                    "Voltar" };
+            option = JOptionPane.showOptionDialog(null,
+                    "Escolha uma opção:",
+                    "Menu",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    options,
+                    null);
+
+            switch (option) {
+                case 0:
+                    // controller.addTeam();
+                    break;
+                case 1:
+                    controller.getAllTeams();
+                    break;
+                case 2:
+                    // controller.getTeamById();
+                    break;
+                case 3:
+                    // controller.updateTeam();
+                    break;
+                case 4:
+                    // controller.deleteTeam();
+                    break;
+                default:
+                    break;
+            }
+        } while (option != -1 && option != 5);
     }
 }
--- src/controller/TeamController.java
+++ src/controller/TeamController.java
@@ -1,11 +1,42 @@
 package controller;
 
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+import exception.NoTeamsException;
+import model.dto.TeamDTO;
 import service.TeamService;
 
 public class TeamController {
+    private static final Logger logger = Logger.getLogger(TeamController.class.getName());
+
     private TeamService service;
 
     public TeamController(TeamService service) {
         this.service = service;
+    }
+
+    public void getAllTeams() {
+        try {
+            Map<Integer, TeamDTO> allTeams = service.getAllTeams();
+
+            StringBuilder message = new StringBuilder("Times disponíveis:\n");
+            for (Map.Entry<Integer, TeamDTO> entry : allTeams.entrySet()) {
+                int id = entry.getKey();
+                TeamDTO team = entry.getValue();
+                message.append("id=").append(id).append(" ").append(team.toString()).append("\n");
+            }
+
+            JOptionPane.showMessageDialog(null, message.toString(), "Times", JOptionPane.INFORMATION_MESSAGE);
+        } catch (NoTeamsException e) {
+            logger.log(Level.WARNING, () -> "Erro ao retornar times: " + e.getMessage());
+            JOptionPane.showMessageDialog(null,
+                    "Erro ao tentar recuperar os times:\n" + e.getMessage(),
+                    "Erro",
+                    JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
